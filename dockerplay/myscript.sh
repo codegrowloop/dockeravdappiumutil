ecf068d2c01b298cd31b29c6baa17f8ab5d08592
freepromtech <freepromsoftware@gmail.com>
2019-10-04 16:01:41 -0500

    Updated

--- dockerplay/myscript.sh
+++ dockerplay/myscript.sh
@@ -2,8 +2,13 @@
 if [ "$(uname)" == "Darwin" ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     export PATH="/usr/local/bin:$PATH"
+    
     brew install docker
-    docker run -p $2:4723 --name $1 85bd49af84a1
+    
+    docker run -e HTTP_PROXY=lum-customer-octi-zone-houston_datacenter-ip-216.158.195.243:xglkj2lklhub@zproxy.lum-superproxy.io:22225 \
+    -e HTTPS_PROXY=lum-customer-octi-zone-houston_datacenter-ip-216.158.195.243:xglkj2lklhub@zproxy.lum-superproxy.io:22225 \
+     -p $2:4723 --name $1 appium/appium-emulator
+     
     docker images
     docker ps
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
